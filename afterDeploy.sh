7141eb450d32d71990f24ace01be795642d061b7
fabian.wilms <fabian.wilms@muenchen.de>
2018-09-25 11:32:44 +0200

    after deploy goal

---
+++ afterDeploy.sh
@@ -0,0 +1,10 @@
+curl https://sentry.io/api/hooks/release/builtin/1287731/e3e441af1b7bf9fc65219130ffcdd8f5bc552e3653de4667339b7b2a6e296273/ \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{
+ "version": $TRAVIS_BUILD_ID,
+ "refs": [{
+ "repository":"FabianWilms/anwctrl",
+ "commit":$TRAVIS_COMMIT
+ }]
+}'
