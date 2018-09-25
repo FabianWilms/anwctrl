3bfbb98a1fabe27c30d67f05c52ee6218f40bbf2
fabian.wilms <fabian.wilms@muenchen.de>
2018-09-25 11:45:14 +0200

    fix script

--- afterDeploy.sh
+++ afterDeploy.sh
@@ -1,10 +1,12 @@
+#!bin/bash
+
 curl https://sentry.io/api/hooks/release/builtin/1287731/e3e441af1b7bf9fc65219130ffcdd8f5bc552e3653de4667339b7b2a6e296273/ \
   -X POST \
   -H 'Content-Type: application/json' \
   -d '{
- "version": $TRAVIS_BUILD_ID,
+ "version": "'"$TRAVIS_BUILD_ID"'",
  "refs": [{
  "repository":"FabianWilms/anwctrl",
- "commit":$TRAVIS_COMMIT
+ "commit":"'"$TRAVIS_COMMIT"'"
  }]
 }'
