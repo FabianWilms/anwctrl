4cfb11c3c651968f1d87144897cc31f210aa56ee
fabian.wilms <fabian.wilms@muenchen.de>
2019-08-27 17:37:39 +0200

    fix sw-notification click

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
 self.addEventListener('notificationclick', function (event) {
     console.log('On notification click: ', event.notification.tag);
     event.notification.close();
-
     // This looks to see if the current is already open and
     // focuses if it is
     event.waitUntil(clients.matchAll({
@@ -10,10 +12,11 @@ self.addEventListener('notificationclick', function (event) {
     }).then(function (clientList) {
         for (var i = 0; i < clientList.length; i++) {
             var client = clientList[i];
-            if (client.url == '/' && 'focus' in client)
-                return client.focus();
+            return client.focus();
         }
         if (clients.openWindow)
             return clients.openWindow('/');
     }));
 });
+
+//alternativ: matchAll({includeUncontrolled: true, type: 'window'}) aus https://stackoverflow.com/questions/35100759/serviceworkers-focus-tab-clients-is-empty-on-notificationclick
